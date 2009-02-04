dfb8d2689e9afe8b6deb5ae6413f85ce5f9eef67
kostas <kostas>
2009-02-04 21:26:59 +0000

    added the stubs for cholesky

--- lib/lapack_stub.cc
+++ lib/lapack_stub.cc
@@ -1,5 +1,5 @@
 // -*- C++ -*-
-// $Id: lapack_stub.cc,v 1.2 2007-10-24 02:41:24 edwards Exp $
+// $Id: lapack_stub.cc,v 1.3 2009-02-04 21:26:59 kostas Exp $
 /*! \file
  *  \brief Stubs for QDP interface to Lapack lib
  */
@@ -66,6 +66,80 @@ namespace QDPLapack
     QDP_error_exit("QDPLapack: %s not implemented", __func__);
     return 0;
   }
+
+
+  
+  
+  int zpotrf(char &uplo, int N,  multi2d<DComplex>& A, int LDA, int& info){
+    
+    QDP_error_exit("QDPLapack: %s not implemented", __func__);
+    return 0;
+
+  }
+  int zpotrf(char &uplo, multi2d<DComplex>& A, int& info){
+    int LDA = A.size1() ; // Assumes square matrix
+    
+    return zpotrf(uplo,LDA,A,LDA,info);
+    
+  }
+
+
+  int cpotrf(char &uplo, int N,  multi2d<Complex>& A, int LDA, int& info){
+    
+    QDP_error_exit("QDPLapack: %s not implemented", __func__);
+    return 0;
+
+  }
+  int cpotrf(char &uplo, multi2d<Complex>& A, int& info){
+    int LDA = A.size1() ; // Assumes square matrix
+    
+    return cpotrf(uplo,LDA,A,LDA,info);
+    
+  }
+  
+
+  
+  int zpotrs(char &uplo, int N, int nrhs,  
+	     multi2d<DComplex>& A, int LDA, 
+	     multi2d<DComplex>& B, int LDB, int& info){
+
+    QDP_error_exit("QDPLapack: %s not implemented", __func__);
+    return 0;
+  }
+  
+  int zpotrs(char &uplo, multi2d<DComplex>& A,  multi2d<DComplex>& B,  
+	     int& info){
+    
+    int LDA = A.size1() ; // Assumes square matrix
+    int LDB = B.size1() ; // second index is fast and its size is size1
+    int nrhs = B.size2() ;
+    
+    zpotrs(uplo,LDA,nrhs,A,LDA,B,LDB,info);
+    
+  }
+
+
+  int cpotrs(char &uplo, int N, int nrhs,  
+	     multi2d<Complex>& A, int LDA, 
+	     multi2d<Complex>& B, int LDB, int& info){
+
+    QDP_error_exit("QDPLapack: %s not implemented", __func__);
+    return 0;
+  
+  }
+  
+  int cpotrs(char &uplo, multi2d<Complex>& A,  multi2d<Complex>& B,  
+	     int& info){
+    
+    int LDA = A.size1() ; // Assumes square matrix
+    int LDB = B.size1() ; // second index is fast and its size is size1
+    int nrhs = B.size2() ;
+    
+    cpotrs(uplo,LDA,nrhs,A,LDA,B,LDB,info);
+    
+  }
+  
+  
   
 } // namespace Lapack
 
